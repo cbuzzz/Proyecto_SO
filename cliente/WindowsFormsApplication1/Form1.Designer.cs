a4e77e8fb6a417ed2ed377e718895596c9ff19af
cbuzzz <sebpinsan12@gmail.com>
2022-12-20 23:30:34 +0100

    version 5

---
+++ cliente/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Cliente
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxUserLog = new System.Windows.Forms.TextBox();
+            this.textBoxPasswordLog1 = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxPasswordLog2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonLogIn = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.buttonSingUp = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(168, 27);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(100, 22);
+            this.textBoxUsername.TabIndex = 7;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(168, 54);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPassword.TabIndex = 8;
+            // 
+            // textBoxUserLog
+            // 
+            this.textBoxUserLog.Location = new System.Drawing.Point(168, 60);
+            this.textBoxUserLog.Name = "textBoxUserLog";
+            this.textBoxUserLog.Size = new System.Drawing.Size(100, 22);
+            this.textBoxUserLog.TabIndex = 9;
+            // 
+            // textBoxPasswordLog1
+            // 
+            this.textBoxPasswordLog1.Location = new System.Drawing.Point(168, 88);
+            this.textBoxPasswordLog1.Name = "textBoxPasswordLog1";
+            this.textBoxPasswordLog1.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPasswordLog1.TabIndex = 10;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(29, 30);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(125, 16);
+            this.labelUsername.TabIndex = 11;
+            this.labelUsername.Text = "Nombre de usuario:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(77, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Contraseña:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Nombre de usuario:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(77, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Contraseña:";
+            // 
+            // textBoxPasswordLog2
+            // 
+            this.textBoxPasswordLog2.Location = new System.Drawing.Point(168, 116);
+            this.textBoxPasswordLog2.Name = "textBoxPasswordLog2";
+            this.textBoxPasswordLog2.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPasswordLog2.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 16);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Confirmar contraseña:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonLogIn);
+            this.groupBox2.Controls.Add(this.textBoxUsername);
+            this.groupBox2.Controls.Add(this.labelUsername);
+            this.groupBox2.Controls.Add(this.textBoxPassword);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(386, 100);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Iniciar Sesión";
+            // 
+            // buttonLogIn
+            // 
+            this.buttonLogIn.Location = new System.Drawing.Point(274, 31);
+            this.buttonLogIn.Name = "buttonLogIn";
+            this.buttonLogIn.Size = new System.Drawing.Size(106, 45);
+            this.buttonLogIn.TabIndex = 19;
+            this.buttonLogIn.Text = "Iniciar Sesión";
+            this.buttonLogIn.UseVisualStyleBackColor = true;
+            this.buttonLogIn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBoxName);
+            this.groupBox3.Controls.Add(this.labelNombre);
+            this.groupBox3.Controls.Add(this.buttonSingUp);
+            this.groupBox3.Controls.Add(this.textBoxPasswordLog1);
+            this.groupBox3.Controls.Add(this.textBoxUserLog);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.textBoxPasswordLog2);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(12, 118);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(386, 149);
+            this.groupBox3.TabIndex = 18;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Registrarse";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(168, 32);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 22);
+            this.textBoxName.TabIndex = 22;
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Location = new System.Drawing.Point(81, 35);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(75, 16);
+            this.labelNombre.TabIndex = 21;
+            this.labelNombre.Text = "Tú nombre:";
+            // 
+            // buttonSingUp
+            // 
+            this.buttonSingUp.Location = new System.Drawing.Point(274, 60);
+            this.buttonSingUp.Name = "buttonSingUp";
+            this.buttonSingUp.Size = new System.Drawing.Size(106, 45);
+            this.buttonSingUp.TabIndex = 20;
+            this.buttonSingUp.Text = "Registrarse";
+            this.buttonSingUp.UseVisualStyleBackColor = true;
+            this.buttonSingUp.Click += new System.EventHandler(this.buttonSingUp_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(416, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 57;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(370, 251);
+            this.dataGridView1.TabIndex = 23;
+            // 
+            // Cliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 277);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Cliente";
+            this.Text = "Cliente";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUserLog;
+        private System.Windows.Forms.TextBox textBoxPasswordLog1;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxPasswordLog2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonLogIn;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button buttonSingUp;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ cliente/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Cliente : Form
+    {
+        Socket server;
+        Thread atender;
+        delegate void DelegadoParaBorrar();
+        delegate void DelegadoParaDGV(string conectados,int i);
+        int puerto = 9060;
+        public Cliente()
+        {
+            InitializeComponent();
+            dataGridView1.ColumnCount = 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+        public void BorraTextBoxRegistrar()
+        {
+            textBoxName.Text = "";
+            textBoxUserLog.Text = "";
+            textBoxPasswordLog1.Text = "";
+            textBoxPasswordLog2.Text = "";
+        }
+        public void BorraTextBoxIniciar()
+        {
+            textBoxUsername.Text = "";
+            textBoxPassword.Text = "";
+        }
+        public void AddRow(string trozo,int i)
+        {
+            int rowID = dataGridView1.Rows.Add();
+            DataGridViewRow row = dataGridView1.Rows[rowID];
+            row.Cells[0].Value = trozo;
+            
+        }
+
+        private void AtenderServidor()
+        {
+            int fin = 0;
+            while (fin ==0)
+            {
+                int codigo;
+                byte[] msg = new byte[80];
+                server.Receive(msg);
+                string mensaje = Encoding.ASCII.GetString(msg).Split('\0')[0];
+                string[] trozos = mensaje.Split('/');
+                codigo = Convert.ToInt32(trozos[0]);
+                DelegadoParaBorrar delegadoBorrarRegistrar = new DelegadoParaBorrar(BorraTextBoxRegistrar);
+                DelegadoParaBorrar delegadoBorrarIniciar = new DelegadoParaBorrar(BorraTextBoxIniciar);
+                DelegadoParaDGV delegadoFila = new DelegadoParaDGV(AddRow);
+
+                switch (codigo)
+                {
+                    case 1:
+                        if (trozos[1] == "Si")
+                        {
+                            MessageBox.Show("Bienvenido " + textBoxUsername.Text + ", has iniciado sesión correctamente");
+                            Invoke(delegadoBorrarIniciar, new object[] { });
+                        }
+                        else if (trozos[1] == "No")
+                        {
+                            MessageBox.Show("El nombre de usuario o contraseña son incorrectos o no existen");
+                            fin = 1;
+                        }
+                        break;
+                    case 2:
+                        if (trozos[1] == "Si")
+                        {
+                            MessageBox.Show("Bienvenido " + textBoxUsername.Text + ", te has registrado correctamente");
+                            Invoke(delegadoBorrarRegistrar, new object[] {});
+                        }
+                        else if (trozos[1] == "No")
+                        {
+                            MessageBox.Show("Ha ocurrido un error al registrarse");
+                            fin = 1;
+                        }
+                        break;
+                    case 3:
+                        
+                        int i;
+                        for (i = 0; i < trozos.Length-1; i++)
+                            Invoke(delegadoFila, new object[] { trozos[i+1],i });
+                        break;
+
+
+
+
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
+            //al que deseamos conectarnos
+            IPAddress direc = IPAddress.Parse("192.168.56.102");
+            IPEndPoint ipep = new IPEndPoint(direc, puerto);
+
+
+            //Creamos el socket 
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);//Intentamos conectar el socket
+                this.BackColor = Color.Green;
+                MessageBox.Show("Conectado");
+
+            }
+            catch (SocketException ex)
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            }
+            if (textBoxUsername.Text == "" || textBoxPassword.Text == "")
+            {
+                MessageBox.Show("Por favor introduzca un nombre de usuario y contraseña");
+            }
+            else
+            {
+                string mensaje = "1/"+ textBoxUsername.Text + "/"+ textBoxPassword.Text;
+                byte[] SQLserver = System.Text.Encoding.ASCII.GetBytes(mensaje);                              
+                server.Send(SQLserver);
+                //byte[] msg2 = new byte[200];
+                //server.Receive(msg2);
+                //string answer = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+
+                ThreadStart ts = delegate{ AtenderServidor(); };
+                atender = new Thread(ts);
+                atender.Start();
+            }
+        }
+
+        private void buttonSingUp_Click(object sender, EventArgs e)
+        {
+            string name = textBoxName.Text;
+            string user = textBoxUserLog.Text;
+            string pass1 = textBoxPasswordLog1.Text;
+            string pass2 = textBoxPasswordLog2.Text;
+            string mensaje;
+            string answer;
+            if (user == "" || pass1 == "" || pass2 == "")
+            {
+                MessageBox.Show("Por favor, rellene todos los campos");
+            }
+            else if (pass1 != pass2) MessageBox.Show("Las contraseñas no coinciden");
+
+            //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
+            //al que deseamos conectarnos
+            IPAddress direc = IPAddress.Parse("192.168.56.102");
+            IPEndPoint ipep = new IPEndPoint(direc, puerto);
+
+
+            //Creamos el socket 
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);//Intentamos conectar el socket
+                this.BackColor = Color.Green;
+                MessageBox.Show("Conectado");
+
+            }
+            catch (SocketException ex)
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            }
+
+            mensaje = "2/" + name + "/" + user + "/" + pass1;
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+            //byte[] msg2 = new byte[200];
+            //server.Receive(msg2);
+            //answer = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+
+            ThreadStart ts = delegate { AtenderServidor(); };
+            atender = new Thread(ts);
+            atender.Start();
+
+        }
+
+    }
+}
